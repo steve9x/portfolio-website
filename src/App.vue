cc2bd83c81d5e4518c1936981c3ccfb17005ba1a
Stephan Brodt <stephan.brodt@volkswagen.de>
2023-01-02 19:12:23 +0100

    css and Project reset

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Hello from the app Component
+  <HelloWorld />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/About.vue'
+import './app.css'
 
 export default {
   name: 'App',
@@ -13,14 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/About.vue
@@ -0,0 +1,10 @@
+<template>
+  Hello To the world
+<h1>Hello H1</h1>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
